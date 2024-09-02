9b9fc288eaed68b2f7906baf0b853d48b80b8a5f
Sarache <sarajotaeme@gmail.com>
2024-09-02 13:11:11 +0200

    commit base name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: "/planet-sara/",
+	base: "/planet-sara-portfolio/",
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
