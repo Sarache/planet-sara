b285293a929453fee53c957f092f60bccc10ee95
Sarache <sarajotaeme@gmail.com>
2024-09-04 12:12:53 +0200

    dist folder created

---
+++ src/components/featuredproject.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="featuredcontent">
+<div class="featuredpicture">
+    <img src="/public/img/Axel_3D.png" alt="">
+    <div class="featuredtext">
+        <div class="featsection">
+            <h4>The client</h4>
+            <p></p>
+        </div>
+        <div class="featsection">
+            <h4>The challenge</h4>
+            <p></p>
+        </div>
+        <div class="featsection">
+            <h4>The solution</h4>
+            <p></p>
+        </div>
+    </div>
+</div>
+</div>
+</template>
+
+
+
+<script>
+import carouselhome from '/src/components/carouselhome.vue';
+
+export default {
+  components: {
+    carouselhome
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/_variables.scss";
+
+
+</style>
